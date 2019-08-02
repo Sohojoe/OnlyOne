a482d2152577644edfa459e465c1631c43025d14
Joe Booth <joe@joebooth.com>
2019-08-03 00:37:57 +0100

    music demo

--- Assets/Scripts/OneCube.cs
+++ Assets/Scripts/OneCube.cs
@@ -1,13 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// using UnityEngine.ProBuilder;
+// using UnityEditor.ProBuilder;
 
 public class OneCube : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        // ProBuilderMesh quad = ProBuilderMesh.Create(
+        //     new Vector3[] {
+        //         new Vector3(0f, 0f, 0f),
+        //         new Vector3(1f, 0f, 0f),
+        //         new Vector3(0f, 1f, 0f),
+        //         new Vector3(1f, 1f, 0f) 
+        //     },
+        //     new Face[] { new Face(new int[] { 0, 1, 2, 1, 3, 2 } ) 
+        // } );        
     }
 
     // Update is called once per frame
